721d9e8107bff19ec1cd599c173eec2b128cedaa
happyleaf.cx <happpyleaf.cx@alibaba-inc.com>
2018-10-24 22:30:22 +0800

    add script

---
+++ config.sh
@@ -0,0 +1,10 @@
+#cd src
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-cscope \
+            --prefix=/usr/local
+#            --with-python-config-dir=/home/happyleaf/python/lib/python2.7/config \
