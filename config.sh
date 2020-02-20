20005b23af1c2203ada30e2bd8c2c22150441381
witcxc <witcxc@gmail.com>
2020-02-20 19:41:40 +0800

    update config.sh

--- config.sh
+++ config.sh
@@ -4,9 +4,6 @@
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
             --with-python-command=/usr/local/bin/python \
-            --enable-python3interp==yes \
-            --with-python3-command=/usr/local/bin/python3.7 \
-            --with-python3-config-dir=/usr/local/opt/python/Frameworks/Python.framework/Versions/3.7/lib/python3.7/config-3.7m-darwin \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-cscope \
@@ -14,3 +11,6 @@
             --enable-gui=gtk2 \
             --prefix=/usr/local
 #            --with-python-config-dir=/home/happyleaf/python/lib/python2.7/config \
+            #--enable-python3interp==yes \
+            #--with-python3-command=/usr/local/bin/python3.7 \
+            #--with-python3-config-dir=/usr/local/opt/python/Frameworks/Python.framework/Versions/3.7/lib/python3.7/config-3.7m-darwin \
